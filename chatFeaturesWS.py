a964f057ebee00889dab6dff11adba48c6e1f36e
joanSolCom <joan.soler.company@gmail.com>
2018-06-18 14:15:41 +0200

    new features

---
+++ chatFeaturesWS.py
@@ -0,0 +1,130 @@
+import os
+from lxml import etree
+import codecs
+from pprint import pprint
+from datetime import datetime
+import time
+import string
+from nltk.tag.stanford import StanfordPOSTagger
+from nltk.corpus import stopwords
+from collections import Counter
+
+class ChatFeatures:
+
+	def __init__ (self, conversation):
+		self.raw = "./raw/11JUN.xml"
+		self.tree = etree.parse(self.raw)
+		self.users = self.tree.xpath("/transcript/messages/message/from//text()")
+		self.userSet = set(self.users)
+		self.msg = self.tree.xpath("/transcript/messages/message/body//text()")
+		self.dates = self.tree.xpath("/transcript/messages/message/date//text()")
+		self.dictMsgPerDay = {}
+
+
+	def clean_words(self, tokens):
+		cleanTokens = []
+		for token in tokens:
+			cleanToken = token
+			for char in string.punctuation:
+				cleanToken = cleanToken.replace(char, "")
+			
+			cleanTokens.append(cleanToken)
+		
+		return cleanTokens
+
+	#### Computes and stores the tokens
+	def getTokens(self, text, date, user):
+		
+		tokens = text.split()
+		tokens = self.clean_words(tokens)
+		date = date.split()[0]
+		self.tokensPerUser = {}
+
+		#### Tokens per day specific
+		if date not in self.dictMsgPerDay:
+			self.dictMsgPerDay[date] = {}
+			if user not in self.dictMsgPerDay[date]:
+				self.dictMsgPerDay[date][user] = []
+			self.dictMsgPerDay[date][user].append(tokens)
+		else:
+			if user not in self.dictMsgPerDay[date]:
+				self.dictMsgPerDay[date][user] = []
+			self.dictMsgPerDay[date][user].append(tokens)
+
+		#### Tokens Per msg general and specific
+		self.tokensPerMsg.append(tokens)
+		if user not in self.tokensPerUser:
+			self.tokensPerUser[user] = []
+
+		self.tokensPerUser[user].append(tokens)
+
+
+	def process(self):
+		i=0
+		self.tokensPerMsg = []
+		self.conversation = {}
+
+		while i<len(self.msg):
+
+			text = self.msg[i]
+			date = self.dates[i]
+			simpleDate = date.split()[0]
+
+			user = self.users[i]
+			if simpleDate not in self.conversation:
+				self.conversation[simpleDate] = []
+
+			self.conversation[simpleDate].append({"user":user,"date":date,"text":text})
+
+			self.getTokens(text, date, user)
+			
+			i+=1
+
+	def turnsPerDay(self):
+		self.turnsPerDayUser = {}
+		self.msgPerDayUser = {}
+
+		for date, listMsgs in self.conversation.iteritems():
+			if date not in self.turnsPerDayUser:
+				self.turnsPerDayUser[date] = {}
+
+			if date not in self.msgPerDayUser:
+				self.msgPerDayUser[date] = {}
+
+			lastUser = None
+			for dictMsg in listMsgs:
+				user = dictMsg["user"]
+				if user not in self.turnsPerDayUser[date]:
+					self.turnsPerDayUser[date][user] = 0
+				if user not in self.msgPerDayUser[date]:
+					self.msgPerDayUser[date][user] = 0
+
+				if user != lastUser:
+					self.turnsPerDayUser[date][user]+=1
+
+				self.msgPerDayUser[date][user]+=1
+				lastUser = user
+
+		return self.msgPerDayUser, self.turnsPerDayUser
+
+	def wordsPerDay(self):
+		stopwordList = stopwords.words('spanish')
+		relevantWordsPerUser = {}
+
+		for day, dictDay in self.dictMsgPerDay.iteritems():
+			if day not in relevantWordsPerUser:
+				relevantWordsPerUser[day] = {}
+
+			for user, messagesUser in dictDay.iteritems():
+				if user not in relevantWordsPerUser[day]:
+					relevantWordsPerUser[day][user] = []			
+
+				for messageUser in messagesUser:
+					for word in messageUser:
+						#if word.lower() not in stopwordList:
+							relevantWordsPerUser[day][user].append(word.lower())
+					
+				relevantWordsPerUser[day][user] = Counter(relevantWordsPerUser[day][user])
+
+		return relevantWordsPerUser
+
---
+++ chatWS.py
@@ -0,0 +1,66 @@
+import os
+from lxml import etree
+import codecs
+from pprint import pprint
+from datetime import datetime
+import time
+import string
+from nltk.tag.stanford import StanfordPOSTagger
+from nltk.corpus import stopwords
+from collections import Counter
+from flask import Flask, app, request, url_for, Response
+from logging.handlers import RotatingFileHandler
+from logging import Formatter, INFO
+import json
+import os
+from flask_jsonpify import jsonpify
+from chatFeaturesWS import ChatFeatures
+
+app = Flask(__name__)
+
+iChat = None
+
+@app.route('/computeChatInfo', methods=['GET'])
+def computeChatInfo():
+	conversation = request.args.get('conversation')
+
+	iChat = ChatFeatures(conversation)
+	iChat.process()
+
+	data = jsonpify(tokenDict)
+	return data
+
+@app.route('/getWordsPerDay', methods=['GET'])
+def getWordsPerDay():
+	conversation = request.args.get('conversation')
+	iChat = ChatFeatures(conversation)
+	iChat.process()
+
+	wpd = iChat.wordsPerDay()
+	data = jsonpify(wpd)
+	return data
+
+@app.route('/getTurnsPerDay', methods=['GET'])
+def turnsPerDayUser():
+	conversation = request.args.get('conversation')
+
+	iChat = ChatFeatures(conversation)
+	iChat.process()
+	tpd = iChat.turnsPerDay()
+	data = jsonpify(tpd)
+	return data
+
+if __name__ == '__main__':
+
+    app.debug = True
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+
+    LOG_FILEPATH = "log.txt"
+
+    formatter = Formatter("[%(asctime)s]\t%(message)s")
+    handler = RotatingFileHandler(LOG_FILEPATH, maxBytes=10000000, backupCount=1)
+    handler.setLevel(INFO)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
