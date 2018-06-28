c6053a2707fdf206a553230fcea9b61fc9e4f833
joanSolCom <joan.soler.company@gmail.com>
2018-06-28 15:21:33 +0200

    added date

---
+++ date.py
@@ -0,0 +1,90 @@
+from chatFeatures import ChatFeatures
+from topic import Topic
+from role import Role
+
+
+class Date:
+
+	def __init__(self):
+		self.iChat = ChatFeatures()
+		self.iChat.process()
+		self.linkFeatsDayUser = self.iChat.linkAnalysis()
+		self.domainWordsDayUser = self.iChat.domainWordsPerDay()
+		self.msgPerTurnDayUser = self.iChat.msgPerTurn()
+		self.quranCitesDayUser = self.iChat.quranCites()
+
+		self.iTopic = Topic()
+
+		#generalRelevance[date] = score
+		self.generalRelevanceDay = self.iTopic.relevantDayDetection()
+		
+		'''
+		self.userRelevanceDay = {}
+		for user in self.iChat.userSet:
+			self.userRelevanceDay[user] = self.iTopic.relevantDayDetection("./stats/distances"+user+".tsv")
+		'''
+
+		self.iRole = Role()
+		self.roleScoresDayUser = self.iRole.computeRolesPerDay()
+
+	def dayReport(self):
+
+		for date, score in self.generalRelevanceDay.iteritems():
+			print "\n===================================="
+			print "REPORT FOR DAY " + date,"\n"
+			print "Day Relevance Score " + str(score)
+
+			'''
+				LINK INFO
+			'''
+			if date in self.linkFeatsDayUser:
+				print "\nLink Info\n"
+				linkInfo = self.linkFeatsDayUser[date]
+				for user, linkList in linkInfo.iteritems():
+					print "\t",user
+					for link, category in linkList:
+						print "\t\t",link,category
+			
+			'''
+				QURAN CITES
+			'''
+			if date in self.quranCitesDayUser:
+				print "\nQuran Cites\n"
+				quranCiteInfo = self.quranCitesDayUser[date]
+				for user, quranDict in quranCiteInfo.iteritems():
+					print "\t",user
+					for tupl in quranDict:
+						print "\t\t",tupl
+
+			'''
+				DOMAIN WORDS
+			'''
+			if date in self.domainWordsDayUser:
+				print "\nDomain Frequencies\n"
+				domainWordInfo = self.domainWordsDayUser[date]
+				for user, dictCategory in domainWordInfo.iteritems():
+					print "\t",user
+					for category, frequency in dictCategory.iteritems():
+						print "\t\t",category,frequency
+
+			'''
+				ROLE SCORES
+			'''
+			if date in self.roleScoresDayUser:
+				print "\nUser Activity Score\n"
+				userRelevance = self.roleScoresDayUser[date]
+				for user, score in userRelevance.iteritems():
+					print "\t",user,score
+
+			'''
+				MSG PER TURN
+			'''
+			if date in self.msgPerTurnDayUser:
+				print "\nMessages per turn\n"
+				msgInfo = self.msgPerTurnDayUser[date]
+				for user, mean in msgInfo.iteritems():
+					print "\t",user,mean
+
+if __name__ == '__main__':
+	iDate = Date()
+	iDate.dayReport()
